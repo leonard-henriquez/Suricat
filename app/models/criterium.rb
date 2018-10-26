34c34a1f2b718fad762043a07eb4d8c2570ebc58
Pierre Lanoe <pierre.lanoe@edhec.com>
2018-10-26 13:48:46 +0200

    change criteria_type in enum criterium model

--- app/models/criterium.rb
+++ app/models/criterium.rb
@@ -1,5 +1,5 @@
 class Criterium < ApplicationRecord
-  enum criteria_type: [:contract_type, :structure, :industry, :job, :geography, :salary]
+  enum type: [:contract_type, :structure, :industry, :job, :geography, :salary]
   belongs_to :user
   validates :type, presence: true
   validates :value, presence: true
