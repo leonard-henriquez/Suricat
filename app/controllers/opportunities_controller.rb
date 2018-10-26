1b11fd31c6b87d1fe93e717dfed86a3556409f88
Adrien Lequiller <adrien.lequiller@edhec.com>
2018-10-26 17:57:03 +0200

    #36 #29 methods opportunities controllers in progress

--- app/controllers/opportunities_controller.rb
+++ app/controllers/opportunities_controller.rb
@@ -1,2 +1,36 @@
 class OpportunitiesController < ApplicationController
+  before_action :set_opportunity, only: [:show]
+  before_action :set_user_opportunity, only [:review, :pending, :applied, :trash]
+  before_action
+  def show
+  end
+
+  def review
+  end
+
+  def pending
+  end
+
+  def applied
+  end
+
+  def trash
+  end
+
+  def destroy
+  end
+
+  private
+
+  def set_opportunity
+    @opportunity = Opportunity.find(params[:id])
+  end
+
+  def set_user_opportunity
+    @user_opportunities = User_opportunity.find(params(opportunity_id))
+  end
+
+  def user_opportunity_params
+    params.require(:user_opportunity).permit(:opportunity_id, :value, :automatic_grade, :personnal_grade, :status)
+  end
 end
