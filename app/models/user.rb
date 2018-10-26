255df1fcfa9f6e9d77944ef303a1ae1e395f0fa0
Pierre Lanoe <pierre.lanoe@edhec.com>
2018-10-26 13:12:08 +0200

    add dependent destroy in user model for user opportunities

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :criteria, dependent: :destroy
   has_many :events, dependent: :destroy
-  has_many :user_opportunities
+  has_many :user_opportunities, dependent: :destroy
   has_many :opportunities, through: :user_opportunities
   has_many :criteria_importance
 end
