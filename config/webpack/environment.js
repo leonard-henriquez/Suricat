a6490df5cf4e7cfafc5d40e7f362c3c7e738e105
Leonard-Henriquez <leonard.henriquez@edhec.com>
2018-10-28 23:50:35 +0100

    Basic components #8

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -20,8 +20,6 @@ module.exports = {
         use: [{
           loader: 'style-loader', // inject CSS to page
         }, {
-          loader: 'css-loader', // translates CSS into CommonJS modules
-        }, {
           loader: 'postcss-loader', // Run post css actions
           options: {
             plugins: function () { // post css plugins, can be exported to postcss.config.js
