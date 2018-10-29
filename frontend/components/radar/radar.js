eb17ab5dc98914cf1388730b402d9f77f1479e01
Leonard-Henriquez <leonard.henriquez@edhec.com>
2018-10-29 17:58:13 +0100

    Created radar component

---
+++ frontend/components/radar/radar.js
@@ -0,0 +1,41 @@
+import "./radar.css";
+import Chart from 'chart.js';
+
+$(document).ready(() => {
+  new Chart(document.getElementById("chartjs"), {
+    "type":"radar",
+    "data": {
+      "labels":["Eating","Drinking","Sleeping","Designing","Coding","Cycling"],
+      "datasets": [
+        {
+          "label":"My criteria",
+          "data": [65,59,90,81,56,55],
+          "fill":true,
+          "backgroundColor":"rgba(255, 99, 132, 0.2)",
+          "borderColor":"rgba(208, 137, 37, 0.67)",
+          "pointBackgroundColor":"rgb(255, 99, 132)",
+          "pointBorderColor":"#fff",
+          "pointHoverBackgroundColor":"#fff",
+          "pointHoverBorderColor":"rgb(255, 99, 132)"
+        }
+      ]
+    },
+    "options": {
+      "elements": {
+        "line": {
+          "tension":0,
+          "borderWidth":3
+        }
+      },
+      "scale": {
+        "ticks": {
+          "beginAtZero": true,
+          "showLabelBackdrop": false
+        },
+        "pointLabels": {
+          "fontSize": 20,
+        }
+      }
+    }
+  });
+})
