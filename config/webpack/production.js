03163fddadbc8d1fc9b30aa16b05eff1458c4fe1
Leonard-Henriquez <leonard.henriquez@edhec.com>
2018-10-29 10:15:40 +0100

    Added Uglify optimization

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -2,4 +2,12 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
 const environment = require('./environment')
 
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
+module.exports = {
+  optimization: {
+    minimizer: [new UglifyJsPlugin()]
+  }
+}
+
 module.exports = environment.toWebpackConfig()
