d8c3685221223e3b4f1a964d3d5f94b908995cdc
Leonard-Henriquez <leonard.henriquez@edhec.com>
2018-10-30 14:04:26 +0100

    Modified models to add delegation on opportunities

--- app/models/user_opportunity.rb
+++ app/models/user_opportunity.rb
@@ -2,6 +2,21 @@ class UserOpportunity < ApplicationRecord
   enum status: [:review, :pending, :applied, :trash]
   belongs_to :user
   belongs_to :opportunity
+  has_one :job, through: :opportunity
+  has_one :company, through: :opportunity
+  has_one :sector, through: :opportunity
+
   validates :automatic_grade, presence: true
   validates :personnal_grade, presence: true
+
+  (Opportunity.attribute_names - self.attribute_names).each do |attr|
+    delegate attr.to_sym, to: :opportunity, allow_nil: true
+  end
+
+  [Job, Company, Sector].each do |obj|
+    (obj.attribute_names - self.attribute_names).each do |attr|
+      object_name = obj.name.underscore
+      delegate attr.to_sym, to: object_name, allow_nil: true, prefix: object_name.to_sym
+    end
+  end
 end
