45a76957b380b908e0af2d5d5b6a5807489d2235
Leonard-Henriquez <leonard.henriquez@edhec.com>
2018-10-30 21:25:52 +0100

    Fixed layout

--- frontend/javascript/application.js
+++ frontend/javascript/application.js
@@ -4,6 +4,7 @@ import "bootstrap";
 import "@fortawesome/fontawesome-free/js/all";
 import "stylesheets/application";
 import "components";
+// import "sticky-sidebar";
 
 import Rails from "rails-ujs";
 import Turbolinks from "turbolinks";
@@ -13,3 +14,17 @@ Turbolinks.start();
 ActiveStorage.start();
 
 require.context('../images', true);
+
+// const stickySidebar = $.fn.stickySidebar.noConflict(); // Returns $.fn.stickySidebar assigned value.
+// $.fn.stickySidebar = stickySidebar; // Give $().stickySidebar functionality.
+// $('#sidebar').stickySidebar({
+//   topSpacing: 60,
+//   bottomSpacing: 60
+// });
+
+// $('document').ready(() => {
+//   const sidebar = new StickySidebar('#sidebar', {
+//     containerSelector: '#content',
+//     minWidth: 350
+//   });
+// });
