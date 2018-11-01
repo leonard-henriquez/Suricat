c1d4d51ff080f3041cbbfda485e4f1478a93795e
Adrien Lequiller <adrien.lequiller@edhec.com>
2018-11-01 12:51:17 +0100

    #25 add js and methods in registration controller

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -6,6 +6,19 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # GET /resource
   def index
+    @user_opportunities = UserOpportunity.where(status: ['pending', 'applied'])
+    @user_opportunities_id = @user_opportunities.map do |user_opportunity|
+      user_opportunity.opportunity_id
+    end
+    @opportunities = Opportunity.where(id: @user_opportunities_id).where.not(latitude: nil, longitude: nil)
+
+    @markers = @opportunities.map do |opportunity|
+      {
+        lat: opportunity.latitude,
+        lng: opportunity.longitude,
+        # infoWindow: { content: render_to_string(partial: "/flats/map_box", locals: { flat: flat }) }
+      }
+    end
   end
 
   # GET /resource/sign_up
