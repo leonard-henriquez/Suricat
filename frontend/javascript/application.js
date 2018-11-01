e4bea289559fc6a86f5d73cd68728b7b15dbb2e8
Pierre Lanoe <pierre.lanoe@edhec.com>
2018-11-01 18:48:04 +0100

    calendar with adding events

--- frontend/javascript/application.js
+++ frontend/javascript/application.js
@@ -12,3 +12,31 @@ import * as ActiveStorage from "activestorage";
 Rails.start();
 Turbolinks.start();
 ActiveStorage.start();
+
+$(document).ready(() => {
+  $('.future,.today').each((i, td) => {
+    $(td).click(() => {
+      const event_name = prompt();
+      const date = $(td).data("date");
+
+      fetch("/events", {
+        method: "POST",
+        headers: {
+          'Content-Type': 'application/json',
+          'X-CSRF-Token': Rails.csrfToken()
+        },
+        credentials: 'same-origin',
+        body: JSON.stringify({ event: { start_time: date, name: event_name } })
+      })
+        .then(response => response.json())
+        .then((data) => {
+          console.log(data); // Look at local_names.default
+          // TODO Ajouter l'event en js dans le HTML
+          const input = `<div class="event-calendar">${event_name}</div>`;
+          td.insertAdjacentHTML("beforeend", input )
+        });
+
+
+    });
+  });
+});
