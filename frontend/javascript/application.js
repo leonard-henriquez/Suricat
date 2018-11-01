530fe8acb3098c6decf57679971f5133389e70b1
Adrien Lequiller <adrien.lequiller@edhec.com>
2018-11-01 15:02:39 +0100

    Add dotenv - Protect my secret data in .env file

--- frontend/javascript/application.js
+++ frontend/javascript/application.js
@@ -4,6 +4,9 @@ import "bootstrap";
 import "@fortawesome/fontawesome-free/js/all";
 import "stylesheets/application";
 import "components";
+import "gmaps";
+
+
 
 import Rails from "rails-ujs";
 import Turbolinks from "turbolinks";
