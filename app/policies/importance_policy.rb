6d2704b8e84f98761015930d200608a60286fd11
Leonard-Henriquez <leonard.henriquez@edhec.com>
2018-11-02 11:40:08 +0100

    Visual fixes on profile

--- app/policies/importance_policy.rb
+++ app/policies/importance_policy.rb
@@ -1,11 +1,11 @@
 class ImportancePolicy < ApplicationPolicy
+  def update?
+    record.user == user
+  end
+
   class Scope < Scope
     def resolve
       scope.where(user: user)
     end
-  end
-
-  def update?
-    record.user == user
   end
 end
