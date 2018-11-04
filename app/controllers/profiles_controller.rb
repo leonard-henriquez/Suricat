319f5b1df74ba02f04daff7b79eae869eab6c946
Adrien Lequiller <adrien.lequiller@edhec.com>
2018-11-04 19:31:22 +0100

    Separated colors for different markers

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,36 +1,30 @@
 class ProfilesController < ApplicationController
 
   def index
-    @user_opportunities_pending = UserOpportunity.where(status: :pending)
-    @user_opportunities_pending_id = @user_opportunities_pending.map do |user_opportunity|
-      user_opportunity.opportunity_id
-    end
-    # @opportunities = Opportunity.where(id: @user_opportunities_id).where.not(latitude: nil, longitude: nil)
-    @opportunities_pending = Opportunity.where(id: @user_opportunities_pending_id.uniq)
-    @markers_pending = @opportunities_pending.map do |opportunity|
+    @markers_pending = @user_opportunities.where(status: :pending).map do |u_op|
+      title = "#{u_op.job_title} @#{u_op.company_name}"
       {
-        lat: opportunity.latitude,
-        lng: opportunity.longitude,
-        status: 'pending',
-        # infoWindow: { content: render_to_string(partial: “/opportunities/map_box”, locals: { flat: flat }) }
+        title: title,
+        lat: u_op.opportunity.latitude,
+        lng: u_op.opportunity.longitude,
+        infoWindow: {
+          content: "<p>#{title}</p>"
+        }
+      }
+    end
+    @markers_applied = @user_opportunities.where(status: :applied).map do |u_op|
+      title = "#{u_op.job_title} @#{u_op.company_name}"
+      {
+        title: title,
+        lat: u_op.opportunity.latitude,
+        lng: u_op.opportunity.longitude,
+        infoWindow: {
+          content: "<p>#{title}</p>"
+        }
       }
     end
 
-    @user_opportunities_applied = UserOpportunity.where(status: :applied)
-    @user_opportunities_applied_id = @user_opportunities_applied.map do |user_opportunity|
-      user_opportunity.opportunity_id
-    end
-    # @opportunities = Opportunity.where(id: @user_opportunities_id).where.not(latitude: nil, longitude: nil)
-    @opportunities_applied = Opportunity.where(id: @user_opportunities_applied_id.uniq)
-    @markers_applied = @opportunities_applied.map do |opportunity|
-      {
-        lat: opportunity.latitude,
-        lng: opportunity.longitude,
-        status: 'applied',
-        # infoWindow: { content: render_to_string(partial: “/opportunities/map_box”, locals: { flat: flat }) }
-      }
-    end
-
-    @markers = @markers_pending << @markers_applied
+    @length_markers_pending = @markers_pending.length
+    @length_markers_applied = @markers_applied.length
   end
 end
--- frontend/javascript/gmaps.js
+++ frontend/javascript/gmaps.js
@@ -4,16 +4,30 @@ const initMap = () => {
   const mapElement = document.getElementById('map');
   if (mapElement) {
     const map = new GMaps({ el: '#map', lat: 0, lng: 0 });
-    const markers = JSON.parse(mapElement.dataset.markers);
-    map.addMarkers(markers);
-    if (markers.length === 0) {
-      map.setZoom(2);
-    } else if (markers.length === 1) {
-      map.setCenter(markers[0].lat, markers[0].lng);
-      map.setZoom(14);
-    } else {
-      map.fitLatLngBounds(markers);
-    }
+    const markers_pending = JSON.parse(mapElement.dataset.pending);
+    const markers_applied = JSON.parse(mapElement.dataset.applied);
+
+    let pinColor = "dd0a35";
+    let pinImage = new google.maps.MarkerImage("http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|" + pinColor);
+    markers_pending.forEach(marker => {
+      marker.icon = pinImage
+      map.addMarker(marker)
+    });
+
+    pinColor = "0b409c";
+    pinImage = new google.maps.MarkerImage("http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|" + pinColor);
+    markers_applied.forEach(marker => {
+      marker.icon = pinImage
+      map.addMarker(marker)
+    });
+    // if (markers.length === 0) {
+    //   map.setZoom(2);
+    // } else if (markers.length === 1) {
+    //   map.setCenter(markers[0].lat, markers[0].lng);
+    //   map.setZoom(14);
+    // } else {
+    //   map.fitLatLngBounds(markers);
+    // }
   }
 };
 
