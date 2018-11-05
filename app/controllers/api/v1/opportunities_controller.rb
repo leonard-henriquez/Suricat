9513db2e8c8859e0255f28b344acb17b957b2544
Leonard-Henriquez <leonard.henriquez@edhec.com>
2018-11-05 09:56:09 +0100

    temp commit

--- app/controllers/api/v1/opportunities_controller.rb
+++ app/controllers/api/v1/opportunities_controller.rb
@@ -1,22 +1,48 @@
+# frozen_string_literal: true
+
 class Api::V1::OpportunitiesController < Api::V1::BaseController
   acts_as_token_authentication_handler_for User
 
   def create
     @user = current_user
+    opportunity = Opportunity.new(
+      job:             Job.first,
+      company:         Company.first,
+      sector:          Sector.first,
+      salary:          1123,
+      job_description: "Ceci est un test",
+      contract_type:   :vie,
+      location:        "Paris",
+      url:             "http://suricat.co/",
+      latitude:        0,
+      longitude:       0
+    )
+    opportunity.save
+    user_opportunity = UserOpportunity.new(
+      user:            @user,
+      opportunity:     opportunity,
+      automatic_grade: 4,
+      personnal_grade: 1,
+      status:          :pending
+    )
+    user_opportunity.save
     if @user
       response = {
-        status: true,
-        user: @user
+        status:           true,
+        user:             @user,
+        user_opportunity: user_opportunity,
+        opportunity:      opportunity
       }
       render json: response
     else
-      response = { status: false, errors: "Unable to find user" }
+      response = {status: false, errors: "Unable to find user"}
       render json: response
     end
   end
 
   private
+
   def params_sessions
-    params.permit()
+    params.permit
   end
 end
