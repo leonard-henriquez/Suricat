90f181a6e1a0fe58fc3e8f1aaca3d52b88d7aa0a
Pierre Lanoe <pierre.lanoe@edhec.com>
2018-11-06 18:52:01 +0100

    autocomplete bad stock in controller

--- frontend/javascript/autocomplete.js
+++ frontend/javascript/autocomplete.js
@@ -1,36 +1,56 @@
-const tagGenerator = (place) => {
+const tagGenerator = (location) => {
   return `
-    <div class="tags">${place.name}X</div>
+    <div id="${location.id}" class="tags">${location.city}</div>
+    <input type="hidden" id="hidden-${location.id}" name="importance[criteria_attributes][0][value][]" value='${JSON.stringify(location)}'>
   `
 }
 
-function autocomplete() {
-  document.addEventListener("DOMContentLoaded", function() {
+const scriptGenerator = (location) => {
+  return `
+    tag = document.getElementById('${location.id}');
+    tag_input = document.getElementById('hidden-${location.id}');
+    tag.addEventListener('click', (e) => {
+      tag.remove();
+      tag_input.remove();
+    });
+  `
+};
+
+const addScript = (location) => {
+  var newScript = document.createElement("script");
+  var inlineScript = document.createTextNode(scriptGenerator(location));
+  newScript.appendChild(inlineScript);
+  document.querySelector('body').appendChild(newScript);
+};
+
+const autocomplete = () => {
+  document.addEventListener("DOMContentLoaded", () => {
     const input = document.getElementById('location');
-    const inputHidden = document.getElementById('cities')
+    let location;
 
     if (input) {
       var autocomplete = new google.maps.places.Autocomplete(input, { types: [ 'geocode' ] });
-      google.maps.event.addDomListener(input, 'keydown', function(e) {
+      google.maps.event.addDomListener(input, 'keydown', (e) => {
         if (e.key === "Enter") {
           e.preventDefault(); // Do not submit the form on Enter.
         }
       });
 
-      autocomplete.addListener('place_changed', function() {
+      autocomplete.addListener('place_changed', () => {
+        input.value = ''
         var place = autocomplete.getPlace();
         console.log(place);
-        input.insertAdjacentHTML('afterend', tagGenerator(place))
-        input.value = ''
-        const locations = JSON.parse(inputHidden.dataset.locations)
-        locations.push({
+
+        location = {
           city: place.name,
+          id: place.place_id,
           lat: place.geometry.location.lat(),
           lng: place.geometry.location.lng()
-        })
-        const string = JSON.stringify(locations)
-        inputHidden.dataset.locations = string
-        inputHidden.value = string
+        };
+
+        input.insertAdjacentHTML('afterend', tagGenerator(location))
+        addScript(location);
+
       });
     }
   })
