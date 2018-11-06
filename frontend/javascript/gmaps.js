8738ea141bf417e511d25f648715f5e9f523fedc
Leonard-Henriquez <leonard.henriquez@edhec.com>
2018-11-06 13:06:11 +0100

    Javacript errors & nil erros

--- frontend/javascript/gmaps.js
+++ frontend/javascript/gmaps.js
@@ -2,72 +2,93 @@ import GMaps from "gmaps/gmaps";
 import blue from "../images/map-marker-blue.svg";
 import red from "../images/map-marker-red.svg";
 
+let map;
+let markers;
+
+const setZoom = () => {
+  if (map === undefined || markers === undefined) {
+    return;
+  }
+
+  if (markers.length === 0) {
+    map.setZoom(2);
+  } else if (markers.length === 1) {
+    map.setZoom(5);
+    map.setCenter({ lat: markers[0].lat, lng: markers[0].lng });
+  } else {
+    map.fitLatLngBounds(markers);
+  }
+};
+
+const onResize = () => {
+  if ($("#map").length) {
+    $("#map").css({ width: "100%", height: "50%" });
+    google.maps.event.trigger(map, "resize");
+    setZoom();
+  }
+};
+
 const initMap = () => {
   const mapElement = document.getElementById("map");
   if (mapElement) {
-    const map = new GMaps({ el: "#map", lat: 0, lng: 0 });
-    const markers = [];
-    if (JSON.parse(mapElement.dataset.pending)) {
+    map = new GMaps({
+      el: "#map",
+      disableDefaultUI: true,
+      lat: 0,
+      lng: 0
+    });
+
+    const iconBlue = {
+      // Adresse de l'icône personnalisée
+      url: blue,
+      // Taille de l'icône personnalisée
+      size: new google.maps.Size(32, 40),
+      // Origine de l'image, souvent (0, 0)
+      origin: new google.maps.Point(0, 0),
+      // L'ancre de l'image. Correspond au point de l'image que l'on raccroche à la carte. Par exemple, si votre îcone est un drapeau, cela correspond à son mâts
+      anchor: new google.maps.Point(16, 40)
+    };
+
+    const iconRed = {
+      url: red,
+      size: new google.maps.Size(32, 40),
+      origin: new google.maps.Point(0, 0),
+      anchor: new google.maps.Point(16, 40)
+    };
+
+    markers = [];
+    let marker;
+    if (
+      mapElement.dataset.pending !== undefined &&
+      mapElement.dataset.applied !== undefined
+    ) {
       const markersPending = JSON.parse(mapElement.dataset.pending);
       const markersApplied = JSON.parse(mapElement.dataset.applied);
 
-      const iconPending = {
-        // Adresse de l'icône personnalisée
-        url: blue,
-        // Taille de l'icône personnalisée
-        size: new google.maps.Size(32, 40),
-        // Origine de l'image, souvent (0, 0)
-        origin: new google.maps.Point(0, 0),
-        // L'ancre de l'image. Correspond au point de l'image que l'on raccroche à la carte. Par exemple, si votre îcone est un drapeau, cela correspond à son mâts
-        anchor: new google.maps.Point(16, 40)
-      };
-
-      const iconApplied = {
-        url: red,
-        size: new google.maps.Size(32, 40),
-        origin: new google.maps.Point(0, 0),
-        anchor: new google.maps.Point(16, 40)
-      };
-
       markersPending.forEach(marker => {
-        marker.icon = iconPending;
+        marker.icon = iconBlue;
         map.addMarker(marker);
+        markers.push(marker);
       });
 
       markersApplied.forEach(marker => {
-        marker.icon = iconApplied;
+        marker.icon = iconRed;
         map.addMarker(marker);
+        markers.push(marker);
       });
 
-      const markersAll = markersPending.concat(markersApplied);
-      markersAll.forEach(hash => {
-        markers.push({ lat: hash.lat, lng: hash.lng });
+      markers.forEach(marker => {
+        markers.push(marker);
       });
-    } else {
-      const marker = JSON.parse(mapElement.dataset.marker);
-      const icon = {
-        url: red,
-        size: new google.maps.Size(32, 40),
-        origin: new google.maps.Point(0, 0),
-        anchor: new google.maps.Point(16, 40)
-      };
-      marker[0].icon = icon;
-      map.addMarker(marker[0]);
-      markers.push({ lat: marker[0].lat, lng: marker[0].lng });
-    }
-    if (markers.length === 0) {
-      map.setZoom(2);
-    } else if (markers.length === 1) {
-      map.setCenter(markers[0].lat, markers[0].lng);
-      map.setZoom(14);
-    } else {
-      map.fitLatLngBounds(markers);
+    } else if (mapElement.dataset.marker !== undefined) {
+      marker = JSON.parse(mapElement.dataset.marker);
+      marker.icon = iconRed;
+      map.addMarker(marker);
+      markers.push(marker);
     }
 
-    $(window).resize(() => {
-      $(mapElement).css({ width: "100%", height: "50%" });
-      google.maps.event.trigger(map, "resize");
-    });
+    $(window).resize(onResize);
+    setTimeout(setZoom, 0);
   }
 };
 
