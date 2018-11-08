d3d360c7f8c422d2932eed6ed877610545895b83
Adrien Lequiller <adrien.lequiller@edhec.com>
2018-11-08 17:01:16 +0100

    added sorting of offers regarding automatic and personnal grade

--- app/controllers/user_opportunities_controller.rb
+++ app/controllers/user_opportunities_controller.rb
@@ -6,8 +6,7 @@ class UserOpportunitiesController < ApplicationController
   def index
     @status = params[:status]
     @user_opportunities_displayed = @user_opportunities.where(status: @status) unless @status.nil?
-    @user_opportunities_displayed = @user_opportunities_displayed.sort_by { |op| op.personnal_grade || 0 }
-    @user_opportunities_displayed = @user_opportunities_displayed.sort_by { |op| op.automatic_grade || 0 }
+    @user_opportunities_displayed = @user_opportunities_displayed.sort_by { |op| [op.personnal_grade, op.automatic_grade] || 0 }.reverse
   end
 
   def show
